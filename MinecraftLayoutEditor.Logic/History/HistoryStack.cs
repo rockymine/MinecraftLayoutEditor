ac6947f350cf12490cba3f8914592ed53ac5ab6d
michael <michael.g186@live.de>
2025-10-17 01:11:20 +0200

    HistoryStack improvement

--- MinecraftLayoutEditor.Logic/History/HistoryStack.cs
+++ MinecraftLayoutEditor.Logic/History/HistoryStack.cs
@@ -1,4 +1,6 @@
-﻿namespace MinecraftLayoutEditor.Logic.History;
+﻿using System.Xml.Linq;
+
+namespace MinecraftLayoutEditor.Logic.History;
 
 public class HistoryStack
 {
@@ -19,23 +21,40 @@ public class HistoryStack
     {
         if (Actions.Count == 0)
             return;
-        
+
         var action = Actions.Pop();
-        
+
         if (action is AddNodeAction addNodeAction)
         {
-            Graph.DeleteNode(addNodeAction.Node);
-
-            //TODO: Fix node edges. In node.edges.othernode add edges. (inverted deletenode)
-        } else if (action is AddEdgeAction addEdgeAction)
+             Graph.DeleteNode(addNodeAction.Node);
+        }
+        else if (action is AddEdgeAction addEdgeAction)
         {
             Graph.AddOrRemoveEdge(addEdgeAction.Node1, addEdgeAction.Node2);
-        } else if (action is AddOrRemoveEdgeAction removeEdgeAction)
+        }
+        else if (action is AddOrRemoveEdgeAction removeEdgeAction)
         {
             Graph.AddOrRemoveEdge(removeEdgeAction.Node1, removeEdgeAction.Node2);
-        } else if (action is RemoveNodeAction removeNodeAction)
+        }
+        else if (action is RemoveNodeAction removeNodeAction)
         {
+            var node = removeNodeAction.Node;
+            var edges = node.Edges;
+
             Graph.AddNode(removeNodeAction.Node);
+
+            // Fix node edges. In node.edges.othernode add edges. (inverted deletenode)
+            foreach (var edge in edges)
+            {
+                if (edge.Node1 != node)
+                {
+                    edge.Node1.Edges.Add(edge);
+                }
+                else if (edge.Node2 != node)
+                {
+                    edge.Node2.Edges.Add(edge);
+                }
+            }
         }
     }
 
