ec753e2f4c8595f8ff90e013a69fd63e95375043
rockymine <rockymine.ocn@gmail.com>
2025-11-09 00:30:43 +0100

    schematic: remove scale parameter

--- MinecraftLayoutEditor.Schematics/SchematicMaker.cs
+++ MinecraftLayoutEditor.Schematics/SchematicMaker.cs
@@ -6,17 +6,17 @@ namespace MinecraftLayoutEditor.Schematics;
 
 public class SchematicMaker
 {
-    public static Schematic FromLayout(Layout layout, int scale = 1)
+    public static Schematic FromLayout(Layout layout)
     {
-        var schematic = new Schematic(layout.Name, (short)(layout.Width * scale), (short)layout.Thickness, 
-            (short)(layout.Height * scale));
+        var schematic = new Schematic(layout.Name, (short)(layout.Width), (short)layout.Thickness, 
+            (short)(layout.Height));
 
-        AddEdgesToSchematic(schematic, layout, scale, schematic.Height, layout.LaneWidth);
+        AddEdgesToSchematic(schematic, layout);
 
         return schematic;
     }
 
-    private static void AddEdgesToSchematic(Schematic schematic, Layout layout, int scale, int height, int laneWidth)
+    private static void AddEdgesToSchematic(Schematic schematic, Layout layout)
     {
         // Collect unique edges to avoid drawing each edge twice
         var uniqueEdges = new HashSet<Edge>();
@@ -33,17 +33,17 @@ public class SchematicMaker
         foreach (var edge in uniqueEdges)
         {
             // Use original world coordinates for rectangle calculation
-            var blocksInside = Rectangle.DiscretePointsInsideRect(edge.Node1.Position, edge.Node2.Position, laneWidth);
+            var blocksInside = Rectangle.DiscretePointsInsideRect(edge.Node1.Position, edge.Node2.Position, layout.LaneWidth);
 
             foreach (var block in blocksInside)
             {
-                var (x, z) = GetSchematicPosition(block, layout.Width, layout.Height, scale);
+                var (x, z) = GetSchematicPosition(block, layout.Width, layout.Height);
 
                 if (x < 0 || x >= schematic.Width || z < 0 || z >= schematic.Length)
                     continue;
 
                 // TODO: Stack layers inside schematic
-                for (int i = 0; i < height; i++)
+                for (int i = 0; i < layout.Thickness; i++)
                 {
                     int blockId = 1;
                     if (i < 2)
@@ -57,13 +57,10 @@ public class SchematicMaker
         // TODO: Find and fill missing corner polygons
     }
 
-    private static (int x, int z) GetSchematicPosition(Vector2 position, int width, int height, int scale)
-    {
-        var scaledX = position.X * scale;
-        var scaledZ = position.Y * scale;
-        
-        var x = (int)Math.Floor(scaledX + (width * scale) / 2);
-        var z = (int)Math.Floor(scaledZ + (height * scale) / 2);
+    private static (int x, int z) GetSchematicPosition(Vector2 position, int width, int height)
+    {  
+        var x = (int)Math.Floor(position.X + width / 2);
+        var z = (int)Math.Floor(position.Y + height / 2);
         return (x, z);
     }
 }
