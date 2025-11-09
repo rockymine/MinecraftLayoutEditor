bf195c6b017442a94c3726a0f0b5926962e764a1
rockymine <rockymine.ocn@gmail.com>
2025-11-09 17:01:09 +0100

    Fix #9: Adjust fit functions to correctly fill the screen; Add support for vertical and horizontal mirror line; Delete unused ResetView

--- MinecraftLayoutEditor.WebApp/Components/ViewControls.razor.cs
+++ MinecraftLayoutEditor.WebApp/Components/ViewControls.razor.cs
@@ -8,13 +8,10 @@ public partial class ViewControls
     [Parameter]
     public bool MirrorEnabled { get; set; }
     [Parameter]
-    public EventCallback OnResetView { get; set; }
-    [Parameter]
     public EventCallback OnFitMap { get; set; }
     [Parameter]
     public EventCallback OnFitTeam { get; set; }
 
-    private Task ResetView() => OnResetView.InvokeAsync();
     private Task FitMap() => OnFitMap.InvokeAsync();
     private Task FitTeam() => OnFitTeam.InvokeAsync();
 }
